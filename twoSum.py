f8e8b5b7642571d1c6adbf2d15d3586fe104be29
Nobodit Choudhury <nobodit12@gmail.com>
2024-04-06 23:38:46 +0530

    Update twoSum.py

--- twoSum.py
+++ twoSum.py
@@ -1,12 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         indices=[]
-        for i in range(len(nums)-1):
-            #print(i": ")
-            for j in range(i+1,len(nums)):
-                #print(j)
-                if nums[i]+nums[j] == target:
-                    indices.append(i)
-                    indices.append(j)
-                    break
-        return indices
+        numsMap={}
+        #build hash table
+        for i in range(len(nums)):
+            numsMap[nums[i]] = i
+
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in numsMap and numsMap[complement]!=i:
+                return [i,numsMap[complement]]
+
+        return []
