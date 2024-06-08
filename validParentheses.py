48865f8d3d5470bc941e456c6ab13e87f69987d7
Nobodit Choudhury <nobodit12@gmail.com>
2024-06-08 13:17:48 +0530

    Create validParentheses.py

---
+++ validParentheses.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) == 1:
+            return False
+        stack = []
+        hm = {'(':')','{':'}','[':']'}
+        for i in s:
+            if i in ['(','[','{'] :
+                stack.append(hm[i])
+            elif i in [')','}',']']:
+                if len(stack) == 0 :
+                    return False
+                elif i != stack.pop():
+                    return False
+            #elif len(stack)!=0 and i == stack[-1]:
+                #stack.pop()
+        if len(stack) == 0 :
+            return True
+        return False
