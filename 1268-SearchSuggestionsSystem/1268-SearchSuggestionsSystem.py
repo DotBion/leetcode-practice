13c9b4ca9f1ddd570ba098a407537d2b183454b1
Nobodit Choudhury <nobodit12@gmail.com>
2025-02-11 17:16:52 -0500

    Create file

---
+++ 1268-SearchSuggestionsSystem/1268-SearchSuggestionsSystem.py
@@ -0,0 +1,16 @@
+class Solution:
+    def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        max = 3
+        products.sort()
+        #print(products)
+        suggestion = []
+        for i in range(1,len(searchWord)+1) :
+            s = searchWord[0:i]
+            stemp = []
+            k=0
+            for p in products :
+                if p.startswith(s) and k<max:
+                    stemp.append(p)
+                    k+=1
+            suggestion.append(stemp)
+        return suggestion
