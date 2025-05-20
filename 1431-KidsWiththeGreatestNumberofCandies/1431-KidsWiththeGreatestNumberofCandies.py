8bacd902e9b7f15f976d6a0d16f9958491c5e53b
Nobodit Choudhury <nobodit12@gmail.com>
2025-05-20 19:34:19 -0400

    File updated at 5/20/2025, 7:34:19 PM

--- 1431-KidsWiththeGreatestNumberofCandies/1431-KidsWiththeGreatestNumberofCandies.py
+++ 1431-KidsWiththeGreatestNumberofCandies/1431-KidsWiththeGreatestNumberofCandies.py
@@ -1,11 +1,19 @@
-# Last updated: 5/20/2025, 7:06:41 PM
+# Last updated: 5/20/2025, 7:34:19 PM
 class Solution:
-    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
-        result = []
-        maxcandies = max(candies)
-        for i in candies :
-            if i+extraCandies >= maxcandies :
-                result.append(True)
-            else :
-                result.append(False)
-        return result
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        prev = flowerbed[0]
+        next = flowerbed[0]
+        for i in range(len(flowerbed)-1):
+            next = flowerbed[i+1]
+            if flowerbed[i] == 0 and prev == 0 and next == 0: #flowerbed[i] == 0 and
+                n-=1
+                flowerbed[i] = 1
+            prev = flowerbed[i]
+        
+        if prev==0 and next==0 :
+            n-=1
+
+        if n>0:
+            return False
+        else :
+            return True
