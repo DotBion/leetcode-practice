0f6a0fbd19122ffbed00ad8f23614902ed0681e6
Nobodit Choudhury <nobodit12@gmail.com>
2025-05-20 19:35:44 -0400

    File updated at 5/20/2025, 7:35:44 PM

--- 1431-KidsWiththeGreatestNumberofCandies/1431-KidsWiththeGreatestNumberofCandies.py
+++ 1431-KidsWiththeGreatestNumberofCandies/1431-KidsWiththeGreatestNumberofCandies.py
@@ -1,8 +1,9 @@
-# Last updated: 5/20/2025, 7:34:19 PM
+# Last updated: 5/20/2025, 7:35:44 PM
 class Solution:
     def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
         prev = flowerbed[0]
         next = flowerbed[0]
+        #checking triplets for 000 takes care of initial start like 00 but not ending x00
         for i in range(len(flowerbed)-1):
             next = flowerbed[i+1]
             if flowerbed[i] == 0 and prev == 0 and next == 0: #flowerbed[i] == 0 and
@@ -10,6 +11,7 @@ class Solution:
                 flowerbed[i] = 1
             prev = flowerbed[i]
         
+        #takes care of ...00 endings
         if prev==0 and next==0 :
             n-=1
 
