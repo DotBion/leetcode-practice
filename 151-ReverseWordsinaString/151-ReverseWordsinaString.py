93d8ab97918e1855cb1acdeeceef2846750508b4
Nobodit Choudhury <nobodit12@gmail.com>
2025-05-21 20:50:30 -0400

    reverting 151-ReverseWordsinaString.py

--- 151-ReverseWordsinaString/151-ReverseWordsinaString.py
+++ 151-ReverseWordsinaString/151-ReverseWordsinaString.py
@@ -1,23 +1,33 @@
-# Last updated: 5/21/2025, 8:44:22 PM
+# Last updated: 5/21/2025, 7:41:10 PM
 class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        pre=[1]
-        post=[1]
-        p = 1
-        for i in range(1,len(nums)) :
-            p = p*nums[i-1]
-            pre.append(p)
-            #post.append(p)
-        print(pre)
-        p=1
-        for i in range(len(nums)-1,0,-1) :
-            p = p*nums[i]
-            post.append(p)
-        #print(post)
-        post.reverse()
-        #print(post)
-        for i in range(len(post)):
-            pre[i] = pre[i]*post[i]
-        return pre
+    def reverseWords(self, s: str) -> str:
+        #double pass
+        # s += " "
+        # word = ""
+        # wordlist = []
+        # lastalpha = 0
+        # for i in s :
+        #     if i == ' ' and lastalpha == 1 :
+        #         wordlist.append(word)
+        #         word=""
+        #         lastalpha = 0
+        #     elif i!=' ' :
+        #         word+=i
+        #         lastalpha = 1
+        # result = ""
+        # for i in range(len(wordlist)-1,-1,-1) :
+        #     result += wordlist[i] + " "
+        # print(result)
+        # return result.rstrip()
 
-             
+        #two pointer
+        # words = s.split()
+        # i,f = 0, len(words) - 1
+        # while i<f :
+        #     words[i], words[f] = words[f], words[i]
+        #     i+=1
+        #     f-=1
+        # return " ".join(words)   
+
+        #one liner
+        return " ".join(reversed((s.split())))  
