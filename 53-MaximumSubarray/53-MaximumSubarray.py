5da8f5acd9e7ae6fb3b34e8dbd4dac47af590261
Nobodit Choudhury <nobodit12@gmail.com>
2025-06-20 00:19:00 -0400

    File updated at 6/20/2025, 12:19:00 AM

--- 53-MaximumSubarray/53-MaximumSubarray.py
+++ 53-MaximumSubarray/53-MaximumSubarray.py
@@ -1,15 +1,14 @@
-# Last updated: 6/20/2025, 12:18:25 AM
+# Last updated: 6/20/2025, 12:19:00 AM
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         #no point in carrying a negative sum since the sum will always reduce!
         maxSum = nums[0]
         right = 0
         sum = 0
-        while right < len(nums)  :
-            
+        while right < len(nums)  :    
             if sum < 0 :
                 sum = 0
-            sum+=nums[right]
+            sum+=nums[right] #so that sum=0 is not taken into account
             if sum>maxSum :
                 maxSum = sum
             right+=1
