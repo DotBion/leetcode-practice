8899865f5542ce12d938a787ae27b4013c4fb872
Nobodit Choudhury <nobodit12@gmail.com>
2025-06-24 20:46:48 -0400

    File updated at 6/24/2025, 8:46:48 PM

--- 48-RotateImage/48-RotateImage.py
+++ 48-RotateImage/48-RotateImage.py
@@ -1,4 +1,4 @@
-# Last updated: 6/24/2025, 7:48:29 PM
+# Last updated: 6/24/2025, 8:46:48 PM
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
         """
@@ -7,9 +7,9 @@ class Solution:
         #step1: swap i,j for only the upper right triangular elements: if done for all will reposition them back tothe prev position
         l = len(matrix[0])
         for i in range(l) :
-            for j in range(l) :
-                if j>=i:
-                    matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+            for j in range(i+1,l) :
+                #if j>=i:
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
         print(matrix)
         #step2: take mirror image of each row
         for i in range(l) :
