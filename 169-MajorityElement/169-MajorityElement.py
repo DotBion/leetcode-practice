41ac0da205377746a7d9bfbc01754b0b214d3fe6
Nobodit Choudhury <nobodit12@gmail.com>
2025-06-25 17:09:30 -0400

    File updated at 6/25/2025, 5:09:30 PM

--- 169-MajorityElement/169-MajorityElement.py
+++ 169-MajorityElement/169-MajorityElement.py
@@ -1,8 +1,7 @@
-# Last updated: 6/25/2025, 5:09:15 PM
+# Last updated: 6/25/2025, 5:09:30 PM
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         nums.sort()
         l = len(nums)
-        if l>1 :
-            return nums[l//2]
-        return nums[0]
+        return nums[l//2]
+        #return nums[0]
