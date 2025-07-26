e4ae153f24e938a30f70d73b5691f8c1ce21a19b
Nobodit Choudhury <nobodit12@gmail.com>
2025-07-25 21:45:59 -0400

    Create 229-MajorityElementII

---
+++ 229-MajorityElementII/229-MajorityElementII.py
@@ -0,0 +1,40 @@
+# Last updated: 7/25/2025, 9:45:59 PM
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        c1, c2 = 0, 0
+        n1, n2 = None, None
+        thresh = len(nums)//3
+        result = []
+        
+        if not nums :
+            return []
+        
+        for num in nums :
+            if c1 == 0 and num!=n2: # can exclude this if ordered later 
+                n1 = num
+                c1+=1
+            elif c2 == 0 and num!=n1:
+                n2 = num
+                c2+=1
+            elif n1 == num :
+                c1+=1
+            elif n2 == num :
+                c2+=1
+            else :
+                c1-=1
+                c2-=1
+        
+        c1, c2 = 0, 0
+        for num in nums :
+            if n1 == num :
+                c1+=1
+            elif n2 == num :
+                c2+=1
+        
+        if c1 > thresh :
+            result.append(n1)
+        if c2 > thresh  :
+            result.append(n2)
+        
+        return sorted(result)
+
