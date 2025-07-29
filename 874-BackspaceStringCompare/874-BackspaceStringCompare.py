d8a990ec07f141f9b8c7778bd23fe357ff391a93
Nobodit Choudhury <nobodit12@gmail.com>
2025-07-29 17:48:15 -0400

    Create 874-BackspaceStringCompare

---
+++ 874-BackspaceStringCompare/874-BackspaceStringCompare.py
@@ -0,0 +1,25 @@
+# Last updated: 7/29/2025, 5:48:14 PM
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        sstack = []
+        tstack = []
+        for i in s :
+            if i == '#':
+                if len(sstack)>0:
+                    sstack.pop()
+            else :
+                sstack.append(i)
+        for i in t :
+            if i == '#':
+                if len(tstack)>0:
+                    tstack.pop()
+            else :
+                tstack.append(i)
+        if len(tstack) == len(sstack) :
+            for i in range(len(tstack)):
+                if tstack.pop() != sstack.pop() :
+                    return False
+            return True
+        return False
+
+            
