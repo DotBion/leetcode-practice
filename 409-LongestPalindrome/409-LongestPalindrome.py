763e5b228ab181027d1572f64688f2837648bad2
Nobodit Choudhury <nobodit12@gmail.com>
2025-07-29 17:48:18 -0400

    Create 409-LongestPalindrome

---
+++ 409-LongestPalindrome/409-LongestPalindrome.py
@@ -0,0 +1,18 @@
+# Last updated: 7/29/2025, 5:48:17 PM
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        #s = s.lower()
+        d = {}
+        f=0
+        l=0
+        for i in s:
+            if i not in d:
+                d[i]=1
+            else:
+                d[i]+=1
+        for i in d.keys():
+            l = l + d[i]
+            if d[i]%2 == 1:
+                f=1
+                l=l-1
+        return l+f
