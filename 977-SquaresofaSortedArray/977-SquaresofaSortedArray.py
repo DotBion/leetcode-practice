de1a669bf61e215e4142175a3e5a850837e3e1bf
Nobodit Choudhury <nobodit12@gmail.com>
2025-08-11 20:41:53 -0400

    File updated at 8/11/2025, 8:41:53 PM

--- 977-SquaresofaSortedArray/977-SquaresofaSortedArray.py
+++ 977-SquaresofaSortedArray/977-SquaresofaSortedArray.py
@@ -1,10 +1,11 @@
-# Last updated: 8/11/2025, 8:41:16 PM
+# Last updated: 8/11/2025, 8:41:53 PM
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
         l = len(nums)
         left, right = 0, l - 1
         squares = [0]*l
         for i in range(l) :
+            
             if abs(nums[left]) < abs(nums[right]) :
                 squares[l-i-1] = nums[right] ** 2
                 right-=1
