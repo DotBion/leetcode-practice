f1edd65b587dc8904578742c56dd85c6dca71190
Nobodit Choudhury <nobodit12@gmail.com>
2025-08-13 15:30:55 -0400

    File updated at 8/13/2025, 3:30:54 PM

--- 643-MaximumAverageSubarrayI/643-MaximumAverageSubarrayI.py
+++ 643-MaximumAverageSubarrayI/643-MaximumAverageSubarrayI.py
@@ -1,17 +1,28 @@
-# Last updated: 8/13/2025, 3:23:41 PM
+# Last updated: 8/13/2025, 3:30:54 PM
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        l = len(nums)
-        for i in range(1,l) :
-            nums[i]+=nums[i-1]
-        res = nums[k-1]
-        left = 1
-        right = k
-        while right<l :
-            res = max(res, nums[right]-nums[left-1])
-            left+=1
+        '''O(n) wo cumulative sum'''
+        res = sum(nums[:k])
+        window_sum = res
+        left, right = 1,k
+        while right<len(nums) :
+            window_sum += nums[right] - nums[left-1]
+            res = max(res,window_sum)
             right+=1
+            left+=1
         return res/k
+        '''O(n)'''
+        # l = len(nums)
+        # for i in range(1,l) :
+        #     nums[i]+=nums[i-1]
+        # res = nums[k-1]
+        # left = 1
+        # right = k
+        # while right<l :
+        #     res = max(res, nums[right]-nums[left-1])
+        #     left+=1
+        #     right+=1
+        # return res/k
 
         # maxAvg = float('-inf')
         # left, right = 0,0+k
