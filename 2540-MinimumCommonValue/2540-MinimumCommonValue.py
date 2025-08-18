625367c37f1660153835b70d26796eb623c7eebe
Nobodit Choudhury <nobodit12@gmail.com>
2025-08-18 09:29:28 -0400

    File updated at 8/18/2025, 9:29:28 AM

--- 2540-MinimumCommonValue/2540-MinimumCommonValue.py
+++ 2540-MinimumCommonValue/2540-MinimumCommonValue.py
@@ -1,17 +1,29 @@
-# Last updated: 8/18/2025, 9:21:38 AM
+# Last updated: 8/18/2025, 9:29:28 AM
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        
-        set1 = set(nums1)
-        for num in nums2 :
-            if num in set1 :
-                return num
-        return -1
+        l1, l2 = 0,0
+
+        while l1<len(nums1) and l2<len(nums2) :
+            if nums1[l1] < nums2[l2] :
+                l1+=1
+            elif nums1[l1] > nums2[l2] :
+                l2+=1
+            else :
+                return nums1[l1]
+
+        return -1 
 
 
 
+        #O(n+m)
+        # set1 = set(nums1)
+        # for num in nums2 : #O(1) since hash
+        #     if num in set1 :
+        #         return num
+        # return -1
 
-        #TLE
+
+        #TLE O(nm)
         # for i in nums1 :
         #     for j in nums2 :
         #         if i == j :
