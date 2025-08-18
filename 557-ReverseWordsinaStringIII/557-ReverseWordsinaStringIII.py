10b9442c587ddc76ef20e5ebe89dced2b0e774ac
Nobodit Choudhury <nobodit12@gmail.com>
2025-08-18 07:32:02 -0400

    File updated at 8/18/2025, 7:32:01 AM

--- 557-ReverseWordsinaStringIII/557-ReverseWordsinaStringIII.py
+++ 557-ReverseWordsinaStringIII/557-ReverseWordsinaStringIII.py
@@ -1,13 +1,30 @@
-# Last updated: 8/17/2025, 10:53:47 AM
+# Last updated: 8/18/2025, 7:32:01 AM
 class Solution:
     def reverseWords(self, s: str) -> str:
-        words = s.split()
-        #print(words)
-        for i in range(len(words)):
-            #print(words[i])
-            words[i] = words[i][::-1]
-        #print(words)
-        return ' '.join(words)
+        
+        result = []
+        space_i = -1
+
+        for i in range(len(s)) :
+            if i==len(s)-1 or s[i]==' ':
+                rev_i = i if i == len(s) - 1 else i-1 #if last chr then i-1, if space then prev el
+                while rev_i > space_i :
+                    result.append(s[rev_i])
+                    rev_i -= 1
+                if i!=len(s) - 1 :#not end of string
+                    result.append(' ')
+                space_i = i
+        return "".join(result)
+
+        
+        
+        # words = s.split() #O(n)
+        # #print(words)
+        # for i in range(len(words)):
+        #     #print(words[i])
+        #     words[i] = words[i][::-1]
+        # #print(words)
+        # return ' '.join(words) #O(n)
 
 
 
