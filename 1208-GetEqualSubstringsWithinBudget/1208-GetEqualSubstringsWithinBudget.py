37ee668008909b9a9a48e2fb90849659180f0bf4
Nobodit Choudhury <nobodit12@gmail.com>
2025-09-05 11:25:50 -0400

    File updated at 9/5/2025, 11:25:50 AM

--- 1208-GetEqualSubstringsWithinBudget/1208-GetEqualSubstringsWithinBudget.py
+++ 1208-GetEqualSubstringsWithinBudget/1208-GetEqualSubstringsWithinBudget.py
@@ -1,22 +1,42 @@
-# Last updated: 9/5/2025, 11:23:06 AM
+# Last updated: 9/5/2025, 11:25:50 AM
 class Solution:
     def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
+        
         cost = []
-        for i in range(len(s)) :
-            cost.append(abs(ord(s[i])-ord(t[i])))
         
         left = 0
         currcost = 0
         maxlen = 0
 
         for right in range(len(s)) :
-            currcost += cost[right]
+            currcost += abs(ord(s[right])-ord(t[right]))
 
             #shrink window until currcost is valid i.e. less than= maxcost
             while currcost > maxCost :
-                currcost -= cost[left]
+                currcost -= abs(ord(s[left])-ord(t[left]))
                 left += 1
             
             maxlen = max(maxlen, right-left+1)
         
         return maxlen
+        
+        
+        # cost = []
+        # for i in range(len(s)) :
+        #     cost.append(abs(ord(s[i])-ord(t[i])))
+        
+        # left = 0
+        # currcost = 0
+        # maxlen = 0
+
+        # for right in range(len(s)) :
+        #     currcost += cost[right]
+
+        #     #shrink window until currcost is valid i.e. less than= maxcost
+        #     while currcost > maxCost :
+        #         currcost -= cost[left]
+        #         left += 1
+            
+        #     maxlen = max(maxlen, right-left+1)
+        
+        # return maxlen
